8dcc3fb106cb4b04934e98cd44bf598e82e021f4
vanessalange <langev@oregonstate.edu>
2021-11-11 00:37:53 -0500

    request microservice built

--- DatabaseService.py
+++ DatabaseService.py
@@ -8,17 +8,36 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def microservice():
     r = requests.get("http://127.0.0.1:5000/")
-    r.json()
+    microservice_dict = dict(r.json())
+    values = list(microservice_dict.values())
+    print(values)
+    microservice_insert(values[0], values[1], values[2], values[3])
     return "Success"
 
 
 def microservice_table():
     conn = sqlite3.connect('archive.db')
     c = conn.cursor()
-    c.execute("""CREATE TABLE entrees (
-                    entree_name text,
-                    price int
+    c.execute("""CREATE TABLE archive (
+                    StringBool int,
+                    FloatBool int,
+                    FloatProperty real,
+                    StringProperty text
                     )""")
+    conn.commit()
+    conn.close()
+
+
+def microservice_insert(StringBool, FloatBool, FloatProperty, StringProperty):
+    conn = sqlite3.connect('archive.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO archive VALUES(:StringBool, :FloatBool, :FloatProperty, :StringProperty)",
+              {"StringBool": StringBool,
+               "FloatBool": FloatBool,
+               "FloatProperty": FloatProperty,
+               "StringProperty": StringProperty
+               }
+              )
     conn.commit()
     conn.close()
 
