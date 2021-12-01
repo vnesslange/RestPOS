9c7bd496fbba1dc0d5ec7c2d4853a257d63f072f
vanessalange <langev@oregonstate.edu>
2021-12-01 01:56:13 -0500

    Now able to add Items to the database on the MGMT screen.

--- mgmt.py
+++ mgmt.py
@@ -5,16 +5,53 @@ import sqlite3
 import DatabaseService
 
 
+def clear(name, price):
+    """Function Clears Text Boxes for Forms"""
+    name.delete(0, END)
+    price.delete(0, END)
+
+
 # Functions for Entrees
 
 def edit_entrees():
     root = Tk()
     root.title("Edit Entrees")
     root.geometry("200x300")
-    Button(root, padx=40, pady=20, text="Add Entree", command="").pack()
+    Button(root, padx=40, pady=20, text="Add Entree", command=add_entrees).pack()
     Button(root, padx=40, pady=20, text="Edit Entree", command="").pack()
     Button(root, padx=40, pady=20, text="Delete Entree", command="").pack()
     Button(root, padx=40, pady=20, text="Display all Entree", command=display_entrees).pack()
+
+
+def add_entrees():
+    root = Tk()
+    root.title("Entrees")
+    root.geometry("400x250")
+    entree_name = Entry(root, width=30)
+    entree_name.grid(row=0, column=1)
+    entree_name_label = Label(root, text="Enter Name:")
+    entree_name_label.grid(row=0, column=0)
+    entree_price = Entry(root, width=30)
+    entree_price.grid(row=1, column=1)
+    entree_price_label = Label(root, text="Enter Price:")
+    entree_price_label.grid(row=1, column=0)
+
+    entree_submit = Button(root, text="Add Entree",
+                           command=lambda: [entree_records(entree_name.get(), entree_price.get()),
+                                            clear(entree_name, entree_price)])
+    entree_submit.grid(row=3, columnspan=2, pady=10, padx=10, ipadx=100)
+
+
+def entree_records(entree_name, price):
+    conn = sqlite3.connect('restPOS.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO entrees VALUES(:entree_name, :price)",
+              {"entree_name": entree_name,
+               "price": price
+               }
+              )
+    conn.commit()
+    conn.close()
 
 
 def display_entrees():
@@ -41,10 +78,40 @@ def edit_apps():
     root = Tk()
     root.title("Edit Apps")
     root.geometry("200x300")
-    Button(root, padx=40, pady=20, text="Add Apps", command="").pack()
+    Button(root, padx=40, pady=20, text="Add Apps", command=add_apps).pack()
     Button(root, padx=40, pady=20, text="Edit Apps", command="").pack()
     Button(root, padx=40, pady=20, text="Delete Apps", command="").pack()
     Button(root, padx=40, pady=20, text="Display all Apps", command=display_apps).pack()
+
+
+def add_apps():
+    root = Tk()
+    root.title("APPs")
+    root.geometry("400x250")
+    app_name = Entry(root, width=30)
+    app_name.grid(row=0, column=1)
+    app_name_label = Label(root, text="Enter Name:")
+    app_name_label.grid(row=0, column=0)
+    app_price = Entry(root, width=30)
+    app_price.grid(row=1, column=1)
+    app_price_label = Label(root, text="Enter Price:")
+    app_price_label.grid(row=1, column=0)
+
+    app_submit = Button(root, text="Add App",
+                        command=lambda: [app_records(app_name.get(), app_price.get()), clear(app_name, app_price)])
+    app_submit.grid(row=3, columnspan=2, pady=10, padx=10, ipadx=100)
+
+
+def app_records(app_name, price):
+    conn = sqlite3.connect('restPOS.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO apps VALUES(:app_name, :price)",
+              {"app_name": app_name,
+               "price": price
+               }
+              )
+    conn.commit()
+    conn.close()
 
 
 def display_apps():
@@ -71,10 +138,41 @@ def edit_drinks():
     root = Tk()
     root.title("Edit Drinks")
     root.geometry("200x300")
-    Button(root, padx=40, pady=20, text="Add Drinks", command="").pack()
+    Button(root, padx=40, pady=20, text="Add Drinks", command=add_drinks).pack()
     Button(root, padx=40, pady=20, text="Edit Drinks", command="").pack()
     Button(root, padx=40, pady=20, text="Delete Drinks", command="").pack()
     Button(root, padx=40, pady=20, text="Display all Drinks", command=display_drinks).pack()
+
+
+def add_drinks():
+    root = Tk()
+    root.title("Drinks")
+    root.geometry("400x250")
+    drinks_name = Entry(root, width=30)
+    drinks_name.grid(row=0, column=1)
+    drinks_name_label = Label(root, text="Enter Name:")
+    drinks_name_label.grid(row=0, column=0)
+    drinks_price = Entry(root, width=30)
+    drinks_price.grid(row=1, column=1)
+    drinks_price_label = Label(root, text="Enter Price:")
+    drinks_price_label.grid(row=1, column=0)
+
+    drinks_submit = Button(root, text="Add Drinks",
+                           command=lambda: [drinks_records(drinks_name.get(), drinks_price.get()),
+                                            clear(drinks_name, drinks_price)])
+    drinks_submit.grid(row=3, columnspan=2, pady=10, padx=10, ipadx=100)
+
+
+def drinks_records(drinks_name, price):
+    conn = sqlite3.connect('restPOS.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO drinks VALUES(:drinks_name, :price)",
+              {"drinks_name": drinks_name,
+               "price": price
+               }
+              )
+    conn.commit()
+    conn.close()
 
 
 def display_drinks():
@@ -101,10 +199,41 @@ def edit_desserts():
     root = Tk()
     root.title("Edit Desserts")
     root.geometry("200x300")
-    Button(root, padx=40, pady=20, text="Add Desserts", command="").pack()
+    Button(root, padx=40, pady=20, text="Add Desserts", command=add_desserts).pack()
     Button(root, padx=40, pady=20, text="Edit Desserts", command="").pack()
     Button(root, padx=40, pady=20, text="Delete Desserts", command="").pack()
     Button(root, padx=40, pady=20, text="Display all Desserts", command=display_desserts).pack()
+
+def add_desserts():
+    root = Tk()
+    root.title("Desserts")
+    root.geometry("400x250")
+    desserts_name = Entry(root, width=30)
+    desserts_name.grid(row=0, column=1)
+    desserts_name_label = Label(root, text="Enter Name:")
+    desserts_name_label.grid(row=0, column=0)
+    desserts_price = Entry(root, width=30)
+    desserts_price.grid(row=1, column=1)
+    desserts_price_label = Label(root, text="Enter Price:")
+    desserts_price_label.grid(row=1, column=0)
+
+    desserts_submit = Button(root, text="Add Desserts",
+                           command=lambda: [desserts_records(desserts_name.get(), desserts_price.get()),
+                                            clear(desserts_name,desserts_price)])
+    desserts_submit.grid(row=3, columnspan=2, pady=10, padx=10, ipadx=100)
+
+
+def desserts_records(desserts_name, price):
+    conn = sqlite3.connect('restPOS.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO desserts VALUES(:desserts_name, :price)",
+              {"desserts_name": desserts_name,
+               "price": price
+               }
+              )
+    conn.commit()
+    conn.close()
+
 
 
 def display_desserts():
